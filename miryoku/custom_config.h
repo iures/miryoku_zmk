29bf11c2fad5d8e80214867c19f799fb32fc0d3b
Iure Sales <iureguimaraes@gmail.com>
2024-04-01 10:52:26 -0400

    wp

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,7 +6,9 @@
 /* U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,              &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \ */
 /* U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,              &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\ */
 /* U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSCP),U_LT(U_NUM, TAB),  U_LT(U_MOUSE, RET),  U_LT(U_SYM, SPACE), U_LT(U_FUN, DEL),  U_NP,            U_NP */
-/**/
+
+
+
 /**/
 /* #define MIRYOKU_LAYER_NUM \ */
 /* U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          \ */
